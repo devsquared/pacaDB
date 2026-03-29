9a7bcb744600721ef5a00bafb47b33c35befd64e
Devin Ward <devin@hey.com>
2026-03-29 16:06:14 -0600

    ch 1: simple Entry struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::time::SystemTime;
+
 fn main() {
-    println!("Hello, world!");
+    let now = SystemTime::now();
+    match now.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(elapsed) => {
+            let seconds_since_epoch: u64 = elapsed.as_secs();
+            let entry = Entry::new("test".to_string(), Some(seconds_since_epoch));
+
+            println!(
+                "value: {}, is_expired: {}",
+                entry.value,
+                entry.is_expired(seconds_since_epoch)
+            );
+            ()
+        }
+        Err(e) => {
+            // This case handles situations where the system time is set before the Unix epoch.
+            eprintln!("SystemTime before UNIX EPOCH! {:?}", e);
+        }
+    }
+}
+
+struct Entry {
+    value: String,
+    expires_at: Option<u64>,
+}
+
+impl Entry {
+    fn new(value: String, expires_at: Option<u64>) -> Entry {
+        Entry { value, expires_at }
+    }
+
+    fn is_expired(&self, now: u64) -> bool {
+        match self.expires_at {
+            Some(exp) => now >= exp,
+            None => false,
+        }
+    }
 }

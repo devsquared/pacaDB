26dcab59111848b9b06bdcc2340d3f7a3c8683ca
Devin Ward <devin@hey.com>
2026-03-30 21:42:06 -0600

    ch3: Arc<Mutex<Store>> shared state

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 mod store;
+use std::sync::{Arc, Mutex};
+
 use crate::store::Store;
 
 fn main() {
-    let mut store = Store::new();
+    let store = Arc::new(Mutex::new(Store::new()));
+    let store_one = Arc::clone(&store);
+    let store_two = Arc::clone(&store);
 
-    store.set("test1".to_string(), "valueA".to_string(), None);
-    store.set("test2".to_string(), "valueB".to_string(), None);
-    store.set("test3".to_string(), "valueC".to_string(), None);
+    let handle_one = std::thread::spawn(move || {
+        store_one
+            .lock()
+            .unwrap()
+            .set("test1".to_string(), "valueA".to_string(), None);
+        store_one
+            .lock()
+            .unwrap()
+            .set("test2".to_string(), "valueB".to_string(), None);
+    });
 
-    let entry = store.get("test1");
+    let handle_two = std::thread::spawn(move || {
+        store_two
+            .lock()
+            .unwrap()
+            .set("testA".to_string(), "value1".to_string(), None);
+        store_two
+            .lock()
+            .unwrap()
+            .set("testB".to_string(), "value2".to_string(), None);
+    });
 
-    println!("{}", entry.unwrap().value())
+    handle_one.join().unwrap();
+    handle_two.join().unwrap();
+
+    let s = store.lock().unwrap();
+    println!("{}", s.get("test1").unwrap().value());
+    println!("{}", s.get("test2").unwrap().value());
+    println!("{}", s.get("testA").unwrap().value());
+    println!("{}", s.get("testB").unwrap().value());
 }
